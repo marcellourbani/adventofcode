ef653daac5fa8f62d8fae170545ee473f62fd4c3
Marcello Urbani <marcello.urbani@gmail.com>
2020-12-02 06:39:25 +0000

    day2 2

--- day2.hs
+++ day2.hs
@@ -2,11 +2,15 @@
 -- stack --resolver lts-10.2 script
 module Main where
 
+import           Data.List.NonEmpty (NonEmpty ((:|)), xor)
 import           Data.List.Split
-
 -- >>> solve $ lines "1-3 a: abcde\n 1-3 b: cdefg\n 2-9 c: ccccccccc"
-solve :: [String] -> Int
-solve = length . filter isValid . fmap parse
+-- (2,1)
+solve :: [String] -> (Int,Int)
+solve l = (val1,val2) where
+    val1 = length . filter isValid $ candidates
+    val2 = length . filter isValid2 $ candidates
+    candidates = parse <$> l
 
 type PwdEntry = (String, Int, Int, Char)
 
@@ -22,6 +26,13 @@ parse s = (pwd,min,max,head char) where
 isValid:: PwdEntry -> Bool
 isValid (pwd,min,max,char) = count >= min && count <= max where
     count = length $ filter (== char) pwd
+
+-- >>> isValid2 ("abcde",1,3,'a')
+-- True
+isValid2:: PwdEntry -> Bool
+isValid2 (pwd,min,max,char) = xor (foundFirst:| [foundSecond]) where
+    foundFirst = pwd !! (min-1) == char
+    foundSecond = pwd !! (max-1) == char
 
 main :: IO ()
 main = interact $ show.solve.lines
