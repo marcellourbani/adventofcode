244f680160d902437e7e68209c42eb562eeef19b
Marcello Urbani <marcello.urbani@gmail.com>
2020-12-06 10:37:34 +0000

    refactor day1

--- day1.hs
+++ day1.hs
@@ -1,26 +1,16 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-10.2 script
 module Main where
-
--- Finds the couples that add up to 2020
--- >>> couples [1721,979,366,299,675,1456]
--- [(1721,299)]
-couples :: [Int] -> [(Int, Int)]
-couples l = [(x,y)| k <- zip l [1..],let x = fst k ,y <- drop (snd k) l,x + y == 2020]
-
--- Finds the triplets that add up to 2020
--- >>> triplets [1721,979,366,299,675,1456]
--- [(979,366,675)]
-triplets :: [Int] -> [(Int, Int, Int)]
-triplets l = [(x,y,z)| k <- zip l [1..],let x = fst k ,y <- drop (snd k) l,z <- drop (snd k + 1) l,x + y + z == 2020]
-
--- finds the couples and triplets that add to 2020, and multiplies them
--- >>> solve [1721,979,366,299,675,1456]
+import           Data.List (tails)
+-- >>> solve [1721,979,366,299,675,1456] -- (514579,241861950)
 -- (514579,241861950)
 solve:: [Int] -> ( Int, Int)
 solve l = (a*b ,c*d*e) where
-    (a,b) = head $ couples l
-    (c,d,e) = head $ triplets l
+    (a,b) = head couples
+    (c,d,e) = head triplets
+    t = tail $ tails l
+    couples = [(x,y)| (x,r)<-zip l t,y<-r, x+y == 2020]
+    triplets = [(x,y,z)|(x,r1,r2)<- zip3 l t (tail t),y<-r1,z<-r2,x+y+z == 2020]
 
 main :: IO ()
-main = interact $ show.solve.fmap read . words
+main = readFile "input/day1.txt" >>= print.solve.fmap read. words
