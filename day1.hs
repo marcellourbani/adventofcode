85572158ea58b509e1ac21e902354bf0a3801f40
Marcello Urbani <marcello.urbani@gmail.com>
2020-12-09 19:41:30 +0000

    refactor day1

--- day1.hs
+++ day1.hs
@@ -5,12 +5,9 @@ import           Data.List (tails)
 -- >>> solve [1721,979,366,299,675,1456] -- (514579,241861950)
 -- (514579,241861950)
 solve:: [Int] -> ( Int, Int)
-solve l = (a*b ,c*d*e) where
-    (a,b) = head couples
-    (c,d,e) = head triplets
-    t = tail $ tails l
-    couples = [(x,y)| (x,r)<-zip l t,y<-r, x+y == 2020]
-    triplets = [(x,y,z)|(x,r1,r2)<- zip3 l t (tail t),y<-r1,z<-r2,x+y+z == 2020]
+solve l = (head couples,head triplets) where
+    couples = [x*y| (x:ys)<-tails l,y<-ys, x+y == 2020]
+    triplets = [x*y*z|(x:ys)<-tails l,(y:zs)<-tails ys,z<-zs,x+y+z == 2020]
 
 main :: IO ()
 main = readFile "input/day1.txt" >>= print.solve.fmap read. words
