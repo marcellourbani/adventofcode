3748040365851817dfc5e2639265eaee5489dc07
Marcello Urbani <marcello.urbani@gmail.com>
2020-12-20 07:12:33 +0000

    day19 part 2 broken

--- day19.hs
+++ day19.hs
@@ -70,6 +70,23 @@ pInput s = Message rules $ lines $ rs !! 1
     rs = splitOn "\n\n" s
     rules = either (const M.empty) M.fromList $ traverse (parse pRuleLine "") $ lines $ head rs
 
+createParser2 :: M.Map Int Rule -> Int -> Parser String
+createParser2 m i = go i <* eof
+  where
+    go :: Int -> Parser String
+    go k = case M.lookup k m of
+      Nothing -> undefined
+      Just (RAtom c) -> string [c]
+      Just (RSeq seqs)
+        | k == 8 -> choice [times n [p42] | n <- [1 .. 100]]
+        | k == 11 -> choice [times n [p42, p31] | n <- [1 .. 100]]
+        | otherwise -> choice $ try . seqParser . fmap go <$> seqs
+        where
+          p42 = go 42
+          p31 = go 31
+          times n l = try $ seqParser $ foldl1 (++) $ replicate n <$> l
+    seqParser = foldl1 (\x y -> (++) <$> x <*> y)
+
 createParser :: M.Map Int Rule -> Int -> Parser String
 createParser m i = go i <* eof
   where
@@ -83,13 +100,26 @@ createParser m i = go i <* eof
 -- >>> solve "0: 4 1 5\n1: 2 3 | 3 2\n2: 4 4 | 5 5\n3: 4 5 | 5 4\n4: \"a\"\n5: \"b\"\n\nababbb\nbababa\nabbbab\naaabbb\naaaabbb"
 -- (["ababbb","abbbab"],5)
 
+-- >>> solve "42: 9 14 | 10 1\n9: 14 27 | 1 26\n10: 23 14 | 28 1\n1: \"a\"\n11: 42 31\n5: 1 14 | 15 1\n19: 14 1 | 14 14\n12: 24 14 | 19 1\n16: 15 1 | 14 14\n31: 14 17 | 1 13\n6: 14 14 | 1 14\n2: 1 24 | 14 4\n0: 8 11\n13: 14 3 | 1 12\n15: 1 | 14\n17: 14 2 | 1 7\n23: 25 1 | 22 14\n28: 16 1\n4: 1 1\n20: 14 14 | 1 15\n3: 5 14 | 16 1\n27: 1 6 | 14 18\n14: \"b\"\n21: 14 1 | 1 14\n25: 1 1 | 1 14\n22: 14 14\n8: 42\n26: 14 22 | 1 20\n18: 15 15\n7: 14 5 | 1 21\n24: 14 1\n\nabbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa\nbbabbbbaabaabba\nbabbbbaabbbbbabbbbbbaabaaabaaa\naaabbbbbbaaaabaababaabababbabaaabbababababaaa\nbbbbbbbaaaabbbbaaabbabaaa\nbbbababbbbaaaaaaaabbababaaababaabab\nababaaaaaabaaab\nababaaaaabbbaba\nbaabbaaaabbaaaababbaababb\nabbbbabbbbaaaababbbbbbaaaababb\naaaaabbaabaaaaababaa\naaaabbaaaabbaaa\naaaabbaabbaaaaaaabbbabbbaaabbaabaaa\nbabaaabbbaaabaababbaabababaaab\naabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"
+-- (3,6)
+
 solve :: String -> (Int, Int)
 solve s = (first, second)
   where
     first = length $ rights $ runParser sparser "" <$> source
-    second = length source
+    second = length $ rights $ runParser sparser2 "" <$> source
     sparser = createParser rules 0
+    sparser2 = createParser rules2 0
+    repeats n l = [foldl1 (++) $ replicate i <$> l | i <- [1 .. n]]
+    rules2 = M.insert 11 (RSeq $ repeats 100 [42, 31]) $ M.insert 8 (RSeq $ repeats 100 [42]) rules
     Message rules source = pInput s
 
 main :: IO ()
 main = readFile "input/day19.txt" >>= print . solve
+
+-- 35: "a"
+-- 72: "b"
+-- 42: 72 122 | 35 29
+-- 31: 35 62 | 72 54
+-- 8: 42 | 42 8
+-- 11: 42 31 | 42 11 31
