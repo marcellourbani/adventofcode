7bc85dd2284a5802b6dbedb39a1c0bdace311fd5
Marcello Urbani <marcello.urbani@gmail.com>
2020-12-26 09:55:43 +0000

    day 20 part 2

--- day20.hs
+++ day20.hs
@@ -6,14 +6,17 @@
 module Main where
 
 import Data.Bits ((.&.))
-import Data.Foldable (find)
+import Data.Bool (bool)
 import Data.List (foldl1', tails, transpose)
 import Data.List.Compat (findIndices)
 import Data.List.Split (splitOn)
 import qualified Data.Map as M
 import qualified Data.Set as S
 
-data Tile = Tile {tid :: Int, ttab :: [[Bool]]} deriving (Show, Eq, Ord)
+data Tile = Tile {tid :: Int, ttab :: [[Bool]]} deriving (Eq, Ord)
+
+instance Show Tile where
+  show (Tile i t) = "Tile " ++ show i ++ ":\n" ++ unlines (map (bool '.' '#') <$> t)
 
 data Orientation = Orientation {flipped :: Bool, rotaton :: Int} deriving (Show, Eq, Ord)
 
@@ -24,10 +27,10 @@ orientations :: [Orientation]
 orientations = [Orientation f n | f <- [False, True], n <- [0 .. 3]]
 
 orientTile :: Tile -> Orientation -> Tile
-orientTile (Tile ti t) (Orientation f r) = Tile ti $ foldr ($) t' (replicate (mod r 4) $transpose . reverse)
+orientTile (Tile ti t) (Orientation fl rot) = Tile ti $ foldr ($) t' (replicate (mod rot 4) $transpose . reverse)
   where
     t'
-      | f = reverse t
+      | fl = reverse t
       | otherwise = t
 
 parse :: String -> [Tile]
@@ -41,9 +44,9 @@ topNum :: Tile -> Int
 topNum = boolsToInt . head . ttab
 
 -- >>> solve "Tile 2311:\n..##.#..#.\n##..#.....\n#...##..#.\n####.#...#\n##.##.###.\n##...#.###\n.#.#.#..##\n..#....#..\n###...#.#.\n..###..###\n\nTile 1951:\n#.##...##.\n#.####...#\n.....#..##\n#...######\n.##.#....#\n.###.#####\n###.##.##.\n.###....#.\n..#.#..#.#\n#...##.#..\n\nTile 1171:\n####...##.\n#..##.#..#\n##.#..#.#.\n.###.####.\n..###.####\n.##....##.\n.#...####.\n#.##.####.\n####..#...\n.....##...\n\nTile 1427:\n###.##.#..\n.#..#.##..\n.#.##.#..#\n#.#.#.##.#\n....#...##\n...##..##.\n...#.#####\n.#.####.#.\n..#..###.#\n..##.#..#.\n\nTile 1489:\n##.#.#....\n..##...#..\n.##..##...\n..#...#...\n#####...#.\n#..#.#.#.#\n...#.#.#..\n##.#...##.\n..##.##.##\n###.##.#..\n\nTile 2473:\n#....####.\n#..#.##...\n#.##..#...\n######.#.#\n.#...#.#.#\n.#########\n.###.#..#.\n########.#\n##...##.#.\n..###.#.#.\n\nTile 2971:\n..#.#....#\n#...###...\n#.#.###...\n##.##..#..\n.#####..##\n.#..####.#\n#..#.#..#.\n..####.###\n..#.#.###.\n...#.#.#.#\n\nTile 2729:\n...#.#.#.#\n####.#....\n..#.#.....\n....#..#.#\n.##..##.#.\n.#.####...\n####.#.#..\n##.####...\n##..#.##..\n#.##...##.\n\nTile 3079:\n#.#.#####.\n.#..######\n..#.......\n######....\n####.#..#.\n.#...#.##.\n#.#####.##\n..#.###...\n..#.......\n..#.###..."
--- (20899048083289,197)
+-- (20899048083289,273)
 
--- solve :: String -> (Int, Int)
+solve :: String -> (Int, Int)
 solve s = (first, second)
   where
     first = product $ fst <$> corners
@@ -56,44 +59,52 @@ solve s = (first, second)
     tileMap = M.fromList [(tid t, t) | t <- tiles]
     corners = filter ((== 4) . S.size . snd) uniqueEdges
     rotate tr r = orientTile tr $ Orientation False r
+    flipTile tr = orientTile tr $ Orientation True 0
     firstCorner = topRight
       where
         (ti, ue) = head corners
-        tt = tileMap M.! ti
+        tt = flipTile $ tileMap M.! ti
         ors = [i | i <- [0 .. 3], let a = topNum $ rotate tt i, S.member a ue]
-        topRight = orientTile tt $ Orientation False $ head [i | i <- ors, mod (i - 1) 4 `elem` ors]
-    toright t = other
+        topRight = orientTile tt $ Orientation False $ head [i | i <- ors, mod (i + 1) 4 `elem` ors]
+    toright t = rt
       where
-        shared = topNum $ rotate t 4
-        toLeft (Orientation f r) = Orientation f $ mod (r -1) 4
-        transform ot = head [orientTile ot (toLeft o) | o <- orientations, topNum (orientTile ot o) == shared]
-        other = fmap transform . flip M.lookup tileMap =<< find (/= tid t) (edgeMap M.! shared)
+        shared = topNum $ rotate t 3
+        nti = [ti | ti <- edgeMap M.! shared, ti /= tid t]
+        nt = [rotate (flipTile t2) 1 | ti <- nti, let t1 = tileMap M.! ti, o <- orientations, let t2 = orientTile t1 o, topNum t2 == shared]
+        rt
+          | null nt = Nothing
+          | otherwise = Just $ head nt
+
     line t = case toright t of
       Nothing -> [t]
       Just t1 -> t : line t1
-    tobelow t = other
+    tobelow t = rt
       where
         shared = topNum $ rotate t 2
-        transform ot = head [orientTile ot o | o <- orientations, topNum (orientTile ot o) == shared]
-        other = fmap transform . flip M.lookup tileMap =<< find (/= tid t) (edgeMap M.! shared)
+        nti = [ti | ti <- edgeMap M.! shared, ti /= tid t]
+        nt = [rotate (flipTile t2) 2 | ti <- nti, let t1 = tileMap M.! ti, o <- orientations, let t2 = orientTile t1 o, topNum t2 == shared]
+        rt
+          | null nt = Nothing
+          | otherwise = Just $ head nt
     column t = case tobelow t of
       Nothing -> [t]
       Just t1 -> t : column t1
-    picture = Tile 0 $ foldl1' (zipWith (++)) $foldl1' (zipWith (++)) <$> (map ttab <$> grid)
+    grid = map clearTile <$> map line (column firstCorner)
       where
         mid = tail . init
         clearTile (Tile i t) = Tile i (mid $ mid <$> t)
-        grid = map clearTile <$> map line (column firstCorner)
+    joingrid = foldl1' (++) . map (foldl1' (zipWith (++)) . map ttab)
+    picture = Tile 0 $ joingrid grid
     monster = map (== '#') <$> lines "                  # \n#    ##    ##    ###\n #  #  #  #  #  #   "
     imonster = boolsToInt <$> monster
-    (mlen, mhei) = (length $head monster, length monster)
+    mlen = length $head monster
     matchline ml l = mlen == length chu && hasml
       where
         chu = take mlen l
         hasml = ml == boolsToInt chu .&. ml
     matinline ml l = findIndices (matchline ml) $ tails l
     countMonsters l = case l of
-      (l1 : l2 : l3 : ls) -> nm + countMonsters (tail l)
+      (l1 : l2 : l3 : _) -> nm + countMonsters (tail l)
         where
           m1 = S.fromList $ matinline (head imonster) l1
           m2 = S.fromList $ matinline (imonster !! 1) l2
@@ -102,8 +113,6 @@ solve s = (first, second)
       _ -> 0
     numMonsters = maximum $ countMonsters . ttab . orientTile picture <$> orientations
     countTrues l = sum $ map (sum . map (\x -> if x then 1 else 0)) l
-
--- findml = findIndex
 
 main :: IO ()
 main = readFile "input/day20.txt" >>= print . solve
