b9e2c138faba868d36b5d515f7d04f499474a02d
Marcello Urbani <marcello.urbani@gmail.com>
2021-12-04 11:52:54 +0000

    day3

--- 2021/day3.hs
+++ 2021/day3.hs
@@ -5,16 +5,16 @@ module Main where
 
 import Data.List (tails, transpose)
 
--- >>> mostFrequent "00100"
--- 0
+-- >>> mostFrequent [0,0,1,0,0,1,1,1]
+-- 1
 
-mostFrequent :: String -> Int
+mostFrequent :: [Int] -> Int
 mostFrequent l
-  | count > mid = 1
-  | otherwise = 0
+  | zeros > ones = 0
+  | otherwise = 1
   where
-    count = sum $ read . (: []) <$> l
-    mid = length l `div` 2
+    ones = sum l
+    zeros = length l - ones
 
 -- >>> toDecimal [1,0,1,1,0]
 -- 22
@@ -24,14 +24,26 @@ toDecimal l = sum [2 ^ exp * x | (x, exp) <- zip l [max, max -1 .. 0]]
     max = length l -1
 
 -- >>> solve [ "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"]
--- 198
+-- (198,230)
 
-solve :: [[Char]] -> Int
-solve l = gamma * epsilon
+solve :: [[Char]] -> (Int, Int)
+solve l = (gamma * epsilon, co2rate * oxyrate)
   where
-    bits = mostFrequent <$> transpose l
+    li = map (read . (: [])) <$> l
+    bits = mostFrequent <$> transpose li
     gamma = toDecimal bits
-    epsilon = 2 ^ length bits - gamma - 1
+    epsilon = toDecimal $ invert bits
+    invert x = (1 -) <$> x
+    oxyrate = toDecimal $ myfilter False 0 li
+    co2rate = toDecimal $ myfilter True 0 li
+    myfilter inverted n l = case l of
+      [] -> []
+      [x] -> x
+      _ -> myfilter inverted (n + 1) filtered
+      where
+        rawcriteria = mostFrequent $ (!! n) <$> l
+        criteria = (if inverted then (1 -) else id) rawcriteria
+        filtered = [x | x <- l, x !! n == criteria]
 
 main :: IO ()
 main = readFile "input/day3.txt" >>= print . solve . words
