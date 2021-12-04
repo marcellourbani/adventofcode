920e312fd3fda6fdf3beecaebd081d85f4d96d88
Marcello Urbani <marcello.urbani@gmail.com>
2021-12-04 09:46:02 +0000

    day2 p2

--- 2021/day2.hs
+++ 2021/day2.hs
@@ -6,16 +6,22 @@ module Main where
 import Data.List (tails)
 
 -- >>> solve [ ("forward", 5), ("down", 5), ("forward", 8), ("up", 3), ("down", 8), ("forward", 2) ]
--- (150,0)
+-- (150,900)
 solve :: [(String, Int)] -> (Int, Int)
-solve l = (x * y, 0)
+solve l = (x * y, y' * x')
   where
     move (x, y) (action, n) = case action of
       "forward" -> (x + n, y)
       "down" -> (x, y + n)
       "up" -> (x, y - n)
       _ -> (x, y)
+    moveWithAim (x, y, aim) (action, n) = case action of
+      "forward" -> (x + n, n * aim + y, aim)
+      "down" -> (x, y, aim + n)
+      "up" -> (x, y, aim - n)
+      _ -> (x, y, aim)
     (x, y) = foldl move (0, 0) l
+    (x', y', _) = foldl moveWithAim (0, 0, 0) l
 
 -- >>> parseLine "down 7"
 -- ("down",7)
