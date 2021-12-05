0784c60110f77792480cdf0db3dd280d45914d2f
Marcello Urbani <marcello.urbani@gmail.com>
2021-12-05 10:26:18 +0000

    day 5 part 1

--- 2021/day5.hs
+++ 2021/day5.hs
@@ -3,6 +3,7 @@
 
 module Main where
 
+import qualified Data.Map.Strict as M
 import Data.Void
 import Text.Megaparsec (ParseErrorBundle, Parsec, between, empty, runParser, some)
 import Text.Megaparsec.Char
@@ -31,10 +32,35 @@ pointParser = Point <$> lexeme L.decimal <* lexeme (char ',') <*> lexeme L.decim
 vectorParser :: Parser Vector
 vectorParser = Vector <$> pointParser <* lexeme (string "->") <*> pointParser
 
+type GameMap = M.Map Int (M.Map Int Int)
+
+gameMapPoint :: GameMap -> Int -> Int -> Int
+gameMapPoint m x y = M.findWithDefault 0 x $ M.findWithDefault M.empty y m
+
+drawPoint :: GameMap -> Point -> GameMap
+drawPoint m (Point x y) = newMap
+  where
+    oldRow = M.findWithDefault M.empty y m
+    oldValue = M.findWithDefault 0 x oldRow
+    newRow = M.insert x (oldValue + 1) oldRow
+    newMap = M.insert y newRow m
+
+drawLine :: GameMap -> Vector -> GameMap
+drawLine m (Vector (Point x1 y1) (Point x2 y2))
+  | x1 == x2 = foldl drawPoint m ys
+  | y1 == y2 = foldl drawPoint m xs
+  | otherwise = m
+  where
+    ys = Point x1 <$> [min y1 y2 .. max y1 y2]
+    xs = Point <$> [min x1 x2 .. max x1 x2] <*> pure y1
+
 -- >>> fmap solve $ parse [ "0,9 -> 5,9", "8,0 -> 0,8", "9,4 -> 3,4", "2,2 -> 2,1", "7,0 -> 7,4", "6,4 -> 2,0", "0,9 -> 2,9", "3,4 -> 1,4", "0,0 -> 8,8", "5,5 -> 8,2" ]
--- Right 1
+-- Right (5,0)
 solve :: [Vector] -> (Int, Int)
-solve _ = (0, 0)
+solve m = (count, 0)
+  where
+    endMap = foldl drawLine M.empty m
+    count = sum $ M.size . M.filter (> 1) <$> endMap
 
 -- >>> parse "0,9 -> 5,9"
 -- 2
