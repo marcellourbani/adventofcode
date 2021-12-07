8c895cafd429c867e7d05edf72e91f6482a70431
Marcello Urbani <marcello.urbani@gmail.com>
2021-12-07 05:44:29 +0000

    day7 p2

--- 2021/day7.hs
+++ 2021/day7.hs
@@ -9,13 +9,17 @@ parse :: String -> [Int]
 parse = map read . splitOn ","
 
 -- >>> solve $ parse "16,1,2,0,4,2,7,1,2,14"
--- (37,[49,41,37,39,41,45,49,53,59,65,71,77,83,89,95,103,111])
-solve :: [Int] -> Int
-solve crabs = minimum fuelcosts
+-- (37,168)
+
+solve :: [Int] -> (Int, Int)
+solve crabs = (minimum fuelcosts, minimum fuelcosts2)
   where
     positions = [minimum crabs .. maximum crabs]
     fuelcost p = sum $ abs . (p -) <$> crabs
     fuelcosts = fuelcost <$> positions
+    distcost a b = x * (x + 1) `div` 2 where x = abs (a - b) -- puzzle text asks for position, but verifies distance
+    fuelcost2 p = sum $ distcost p <$> crabs
+    fuelcosts2 = fuelcost2 <$> positions
 
 main :: IO ()
 main = readFile "input/day7.txt" >>= print . solve . parse
