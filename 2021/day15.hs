a28b3545fbb1d2ffdbee9c6b86ade5276c38739b
Marcello Urbani <marcello.urbani@gmail.com>
2021-12-15 08:07:23 +0000

    day15 p1

--- 2021/day15.hs
+++ 2021/day15.hs
@@ -1,7 +1,11 @@
+#!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 
 module Main where
 
+import qualified Data.Map.Strict as M
+import Data.Maybe (catMaybes)
+import qualified Data.Set as S
 import qualified Data.Vector as V
 
 type Grid = V.Vector (V.Vector Int)
@@ -19,26 +23,34 @@ parse s = Input {maxx = mx, maxy = my, grid = V.fromList $ V.fromList <$> depthl
     my = length depthlist - 1
     mx = length (head depthlist) - 1
 
-minPath :: Input -> (Int, [(Int, Int)])
-minPath (Input mx my gr) = go 0 0 (0, [])
+dijkstra :: Input -> M.Map (Int, Int) Int
+dijkstra (Input mx my gr) = go initial
   where
-    go x y acc = case (x == mx, y == my) of
-      (True, True) -> acc
-      (True, _) -> go x (y + 1) acc'
-      (_, True) -> go (x + 1) y acc'
-      _ -> if fst cx < fst cy then cx else cy
-        where
-          cx = go (x + 1) y acc'
-          cy = go x (y + 1) acc'
+    go m = if (mx + 1) * (my + 1) > M.size m then go $step m else m
+    initial = M.singleton (0, 0) 0
+    step m = M.union m newnodes
       where
-        acc' = (fst acc + curcost, (x, y) : snd acc)
-        curcost = gridAt gr x y
+        visited = M.keysSet m
+        tovisit = S.difference (S.unions $ neighbors <$> S.toList visited) visited
+        newnodes = M.fromList $ cost <$> S.toList tovisit
+        cost (a, b) = ((a, b), mincost + gridAt gr a b)
+          where
+            known = S.toList $ neighbors (a, b)
+            mincost = minimum . catMaybes $ M.lookup <$> known <*> pure m
+        neighbors (a, b) = S.fromList [(x, y) | x <- [mina .. maxa], y <- [minb .. maxb], (x, y) /= (a, b), a == x || b == y]
+          where
+            minb = max 0 $ b - 1
+            maxb = min my $ b + 1
+            mina = max 0 $ a - 1
+            maxa = min mx $ a + 1
 
--- >>> minPath  $parse "1163751742\n1381373672\n2136511328\n3694931569\n7463417111\n1319128137\n1359912421\n3125421639\n1293138521\n2311944581"
--- (40,[(9,8),(8,8),(8,7),(8,6),(8,5),(7,5),(7,4),(7,3),(6,3),(6,2),(5,2),(4,2),(3,2),(2,2),(1,2),(0,2),(0,1),(0,0)])
+-- >>> solve  $parse "1163751742\n1381373672\n2136511328\n3694931569\n7463417111\n1319128137\n1359912421\n3125421639\n1293138521\n2311944581"
+-- (40,0)
 
 solve :: Input -> (Int, Int)
-solve i = (fst $ minPath i, 0)
+solve (Input mx my g) = (distances M.! (mx, my), 0)
+  where
+    distances = dijkstra (Input mx my g)
 
 main :: IO ()
 main = readFile "input/day15.txt" >>= print . solve . parse
