6ee44400960794e3b4abacf60bd9e70c716db078
Marcello Urbani <marcello.urbani@gmail.com>
2021-12-19 09:53:36 +0000

    day18 p2

--- 2021/day18.hs
+++ 2021/day18.hs
@@ -119,10 +119,13 @@ magnitude (FN a b) = 3 * go a + 2 * go b
     go (CN n) = magnitude n
 
 -- >>> solve  $parse "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]\n[[[5,[2,8]],4],[5,[[9,9],0]]]\n[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]\n[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]\n[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]\n[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]\n[[[[5,4],[7,7]],8],[[8,3],8]]\n[[9,3],[[9,9],[6,[4,9]]]]\n[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]\n[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"
--- 4140
+-- (4140,3993)
 
-solve :: Input -> Int
-solve i = magnitude $ sumNums i
+solve :: Input -> (Int, Int)
+solve i = (p1, p2)
+  where
+    p1 = magnitude $ sumNums i
+    p2 = maximum [magnitude $addNums a b | a <- i, b <- i]
 
 main :: IO ()
 main = readFile "input/day18.txt" >>= print . solve . parse
