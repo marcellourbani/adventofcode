aa47b3688f20bba75a4ef41cc4f48bb2923887f0
Marcello Urbani <marcello.urbani@gmail.com>
2022-01-13 13:13:47 +0000

    refactor

--- 2015/day12.hs
+++ 2015/day12.hs
@@ -6,32 +6,23 @@
 module Main where
 
 import Data.Aeson
-import qualified Data.Aeson.Types as T
 import qualified Data.ByteString.Lazy as B
 import Data.Maybe (fromMaybe)
-import Data.Text (Text)
 
 parse :: B.ByteString -> Value
 parse i = fromMaybe Null $ decode i
 
-sumNumbers :: Value -> Int
-sumNumbers v = case v of
-  Object o -> sum $ sumNumbers <$> o
-  Array a -> sum $ sumNumbers <$> a
-  Number n -> floor n
-  _ -> 0
-
-sumNumbers2 :: Value -> Int
-sumNumbers2 v = case v of
-  Object o | String "red" `notElem` o -> sum $ sumNumbers2 <$> o
-  Array a -> sum $ sumNumbers2 <$> a
+sumNumbers :: (Object -> Bool) -> Value -> Int
+sumNumbers f v = case v of
+  Object o | f o -> sum $ sumNumbers f <$> o
+  Array a -> sum $ sumNumbers f <$> a
   Number n -> floor n
   _ -> 0
 
 -- >>> solve $ parse "{\"a\":1, \"b\":2,\"r\":\"red\"}"
 -- (3,0)
-
-solve l = (sumNumbers l, sumNumbers2 l)
+solve :: Value -> (Int, Int)
+solve l = (sumNumbers (const True) l, sumNumbers noRed l) where noRed o = String "red" `notElem` o
 
 main :: IO ()
 main = B.readFile "input/day12.txt" >>= print . solve . parse
