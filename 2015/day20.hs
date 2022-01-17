bdcbb2e36220ac2850337f41af56492bbe7a2b5d
Marcello Urbani <marcello.urbani@gmail.com>
2022-01-17 11:56:52 +0000

    day 20 p2

--- 2015/day20.hs
+++ 2015/day20.hs
@@ -11,14 +11,17 @@ parse :: String -> Int
 parse = read
 
 -- >>> solve $ parse "150"
--- 8
+-- (8,8)
 
--- solve :: Int -> Int
-solve l = minimum $ M.keys sols
+solve :: Int -> (Int, Int)
+solve l = (minimum $ M.keys sols, minimum $ M.keys sols2)
   where
     target = div l 10
     sols = M.filter (>= target) $ foldr' f M.empty [1 .. target `div` 2 + 1]
     f x y = M.unionWith (+) y nm where nm = M.fromList $ zip [x, x + x .. target] $ repeat x
+    target2 = div l 11 + mod l 11
+    f2 x y = M.unionWith (+) y nm where nm = M.fromList $ zip [x, x + x .. 50 * x] $ repeat x
+    sols2 = M.filter (>= target2) $ foldr' f2 M.empty [1 .. target2 `div` 2 + 1]
 
 main :: IO ()
 main = readFile "input/day20.txt" >>= print . solve . parse
