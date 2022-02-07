d50fb24bdcc80633da5cb1436628373404836276
Marcello Urbani <marcello.urbani@gmail.com>
2022-02-07 12:14:03 +0000

    d 10

--- 2017/day10.hs
+++ 2017/day10.hs
@@ -3,16 +3,17 @@
 
 module Main where
 
+import Control.Lens (re, (^.))
+import Data.Bits (xor)
+import Data.Char (chr)
 import Data.Foldable (Foldable (foldl'))
 import Data.List.Split (splitOn)
+import Numeric.Lens (hex)
 
 data State = State {slist :: [Int], sindex :: Int, sskip :: Int} deriving (Show, Eq)
 
-parse :: String -> [Int]
-parse s = read <$> splitOn "," s
-
--- >>> solve 4 $ parse "3, 4, 1, 5"
--- ([0,1,2,3,4],[3,4,1,5])
+parse :: String -> ([Int], [Int])
+parse s = (read <$> splitOn "," s, fromEnum <$> s)
 
 move :: State -> Int -> State
 move (State l i s) n = State l''' i' (s + 1)
@@ -23,14 +24,27 @@ move (State l i s) n = State l''' i' (s + 1)
     l'' = reverse (take n l') ++ drop n l'
     l''' = drop (ll - i) l'' ++ take (ll - i) l''
 
--- >>> solve 4 $ parse "3, 4, 1, 5"
--- 12
+-- >>> solve 4 $ parse "3,4,1,5"
+-- >>> solve 255 $ parse "1,2,3"
+-- (12,"04")
+-- (0,"3efbe78a8d82f29979031a4aa0b16a9d")
 
-solve :: Int -> [Int] -> Int
-solve n l = a * b
+solve :: Int -> ([Int], [Int]) -> (Int, String)
+solve n (l, l2) = (a * b, p2)
   where
     (State (a : b : _) _ _) = foldl' move initial l
     initial = State [0 .. n] 0 0
+    h v
+      | v >= 16 = v ^. re hex
+      | otherwise = '0' : v ^. re hex
+    p2 = reduce sparse >>= h
+      where
+        sparse = slist $ foldl' move initial l2'
+        base = l2 <> [17, 31, 73, 47, 23]
+        l2' = take (64 * length base) $ cycle base
+        reduce l = case take 16 l of
+          [] -> []
+          l' -> foldl' xor 0 l' : reduce (drop 16 l)
 
 main :: IO ()
 main = readFile "input/day10.txt" >>= print . solve 255 . parse
