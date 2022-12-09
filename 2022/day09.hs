1bddec0433621d1a9ecb17084ce7c44a25229f7a
Marcello Urbani <marcello.urbani@gmail.com>
2022-12-09 10:05:29 +0000

    2022 d9 p2

--- 2022/day09.hs
+++ 2022/day09.hs
@@ -5,13 +5,12 @@ module Main where
 
 import Data.Foldable (Foldable (foldl'))
 import qualified Data.Set as S
-import Debug.Trace (trace)
 
 data Direction = L | U | R | D deriving (Read, Show)
 
 type Move = (Direction, Int)
 
-data State = State {sHead :: (Int, Int), sTail :: (Int, Int), sVis :: S.Set (Int, Int)} deriving (Show)
+data State = State {sHead :: (Int, Int), sTail :: [(Int, Int)], sVis :: S.Set (Int, Int)} deriving (Show)
 
 parse :: String -> [Move]
 parse s = readline . words <$> lines s
@@ -19,21 +18,33 @@ parse s = readline . words <$> lines s
     readline [a, b] = (read a, read b)
     readline _ = undefined
 
+follow :: (Int, Int) -> (Int, Int) -> (Int, Int)
+follow h@(hx, hy) (tx, ty) = case (tx' == tx, ty' == ty) of
+  (True, False) | tx' /= hx -> (hx, ty')
+  (False, True) | ty' /= hy -> (tx', hy)
+  (_, _) -> (tx', ty')
+  where
+    (tx', ty') = (moveTail hx tx, moveTail hy ty)
+    moveTail hc tc = if abs d > 1 then tc + div d (abs d) else tc where d = hc - tc
+
+touching :: (Int, Int) -> (Int, Int) -> Bool
+touching h@(hx, hy) (tx, ty) = abs (hx - tx) <= 1 && abs (hy - ty) <= 1
+
 evolve :: State -> Move -> State
-evolve s@(State h@(hx, hy) (tx, ty) vis) m@(dir, ml) = case (ml, touching) of
-  (0, True) -> s
+evolve s@(State h@(hx, hy) t vis) m@(dir, ml) = case (ml, joined $ h : t) of
+  (0, True) -> s'
   (0, False) -> evolve (s {sTail = t', sVis = vis'}) m
   (_, _) -> evolve s' (dir, ml -1)
   where
     moveTail hc tc = if abs d > 1 then tc + div d (abs d) else tc where d = hc - tc
-    t' = case (tx' == tx, ty' == ty) of
-      (True, False) | tx' /= hx -> (hx, ty')
-      (False, True) | ty' /= hy -> (tx', hy)
-      (_, _) -> (tx', ty')
-      where
-        (tx', ty') = (moveTail hx tx, moveTail hy ty)
-    vis' = S.insert t' vis
-    touching = abs (hx - tx) <= 1 && abs (hy - ty) <= 1
+    ft he ta = case ta of
+      [] -> []
+      x : xs -> follow he x : ft x xs
+    joined l = case l of
+      a : b : xs -> touching a b && joined (b : xs)
+      _ -> True
+    t' = ft h t
+    vis' = S.insert (last t) vis
     h'@(hx', hy') = case m of
       (_, 0) -> h
       (L, _) -> (hx -1, hy)
@@ -43,15 +54,17 @@ evolve s@(State h@(hx, hy) (tx, ty) vis) m@(dir, ml) = case (ml, touching) of
     s' = State h' t' vis'
 
 -- >>> solve $ parse "R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2"
--- 13
+-- >>> solve $ parse "R 5\nU 8\nL 8\nD 3\nR 17\nD 10\nL 25\nU 20"
+-- (13,1)
+-- (88,36)
 
-solve :: [Move] -> Int
-solve l = p1
+solve :: [Move] -> (Int, Int)
+solve l = (p1, p2)
   where
-    initial = State (0, 0) (0, 0) $ S.singleton (0, 0)
+    initial = State (0, 0) [(0, 0)] $ S.singleton (0, 0)
+    initial2 = initial {sTail = replicate 9 (0, 0)}
     p1 = S.size . sVis $ foldl' evolve initial l
-    ll = parse "R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2"
-    p1b = foldl' evolve initial $ take 2 ll
+    p2 = S.size . sVis $ foldl' evolve initial2 l
 
 main :: IO ()
 main = readFile "input/day09.txt" >>= print . solve . parse
