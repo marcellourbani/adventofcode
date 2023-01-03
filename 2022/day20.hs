04626c67c8bdcda0dabaf1297f8b85dfb48209b9
Marcello Urbani <marcello.urbani@gmail.com>
2023-01-03 23:47:15 +0000

    2022 day20 p2

--- 2022/day20.hs
+++ 2022/day20.hs
@@ -25,19 +25,20 @@ mix cl i = case C.findRotateTo ((== i) . fst) cl of
   Nothing -> cl
   Just cl' -> case C.focus cl' of
     Nothing -> cl
-    Just (_, shi) | mod shi (C.size cl) == 0 -> cl
-    Just c@(_, shi)
-      | shi > 0 -> C.insertL c $ C.rotNR shi $ C.removeL cl'
-      | shi == 0 -> cl
-      | otherwise -> C.insertL c $ C.rotNL (abs shi) $ C.removeL cl'
+    Just c@(_, shi) -> C.insertL c $ C.rotNR shi' $ C.removeL cl'
+      where
+        shi' = mod shi $ C.size cl - 1
 
 -- >>> solve $ parse "1\n2\n-3\n3\n-2\n0\n4"
--- 3
+-- ([4,-3,2],[811589153,2434767459,-1623178306])
 
-solve l = p1
+solve :: [Int] -> (Int, Int)
+solve l = (p1, p2)
   where
     initial = C.fromList $ zip [0 ..] l
+    initial2 = C.fromList $ zip [0 ..] $ (* 811589153) <$> l
     p1 = groveC $ snd <$> go initial [0 .. length l]
+    p2 = groveC $ snd <$> go initial2 (concat $ replicate 10 [0 .. length l])
     go ll is = case is of
       [] -> ll
       x : xs -> go (mix ll x) xs
