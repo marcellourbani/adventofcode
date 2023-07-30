6a6c076f50519ab26c7643aa952584c938348e0a
Marcello Urbani <marcello.urbani@gmail.com>
2023-07-30 23:41:54 +0100

    2022 d18 p2

--- 2022/day18.hs
+++ 2022/day18.hs
@@ -1,6 +1,8 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 
+{-# LANGUAGE NoImportQualifiedPost #-}
+
 module Main where
 
 import Data.List (sort)
@@ -12,32 +14,32 @@ parse s = S.fromList $ read <$> coords
     coords = (<> ")") . ('(' :) <$> lines s
 
 neighbors :: (Int, Int, Int) -> S.Set (Int, Int, Int)
-neighbors (x, y, z) = S.fromList [(x + 1, y, z), (x -1, y, z), (x, y + 1, z), (x, y -1, z), (x, y, z + 1), (x, y, z -1)]
+neighbors (x, y, z) =
+  S.fromList [(x + 1, y, z), (x - 1, y, z), (x, y + 1, z), (x, y - 1, z), (x, y, z + 1), (x, y, z - 1)]
 
 exposed :: S.Set (Int, Int, Int) -> (Int, Int, Int) -> Int
 exposed cubes cube = 6 - S.size (S.intersection cubes $ neighbors cube)
 
 external :: S.Set (Int, Int, Int) -> Int
-external cubes = 2 * (top + front + side)
+external voxels = go (S.singleton (-1, -1, -1)) S.empty 0
   where
-    lcubes = S.toList cubes
-    cx (x, _, _) = x
-    cy (_, y, _) = y
-    cz (_, _, z) = z
-    lim f = (f $ cy <$> lcubes, f $ cy <$> lcubes, f $ cz <$> lcubes)
-    (minx, miny, minz) = lim minimum
-    (maxx, maxy, maxz) = lim maximum
-
-    has f = not $ S.null $ S.filter f cubes
-    topfilt (x, y) (x', y', _) = x == x' && y == y'
-    top = length $ filter id $ has . topfilt <$> [(a, b) | a <- [minx .. maxx], b <- [miny .. maxy]]
-    sidefilt (y, z) (_, y', z') = z == z' && y == y'
-    side = length $ filter id $ has . sidefilt <$> [(a, b) | a <- [miny .. maxy], b <- [minz .. maxz]]
-    frontfilt (x, z) (x', _, z') = z == z' && x == x'
-    front = length $ filter id $ has . frontfilt <$> [(a, b) | a <- [minx .. maxx], b <- [minz .. maxz]]
+    inbox (x, y, z) = and [x >= -1, x <= 22, y >= -1, y <= 22, z >= -1, z <= 22]
+    go current visited tot =
+      if S.null current
+        then tot
+        else go current' visited' tot'
+      where
+        valid c = S.notMember c visited && inbox c
+        curneighbors = S.filter valid . neighbors <$> S.toList current
+        newfaces = sum $ S.size . S.intersection voxels <$> curneighbors
+        newcandidates = S.unions curneighbors
+        onedge = voxels `S.intersection` newcandidates
+        current' = newcandidates `S.difference` onedge
+        visited' = visited `S.union` current'
+        tot' = tot + newfaces
 
 -- >>> solve $ parse "2,2,2\n1,2,2\n3,2,2\n2,1,2\n2,3,2\n2,2,1\n2,2,3\n2,2,4\n2,2,6\n1,2,5\n3,2,5\n2,1,5\n2,3,5"
--- (64,50)
+-- (64,58)
 
 solve :: S.Set (Int, Int, Int) -> (Int, Int)
 solve l = (p1, p2)
