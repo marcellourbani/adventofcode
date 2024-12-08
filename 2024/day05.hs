2265b3b832cc4c277ed9ea8f20e55a9e83dbcc5f
Marcello Urbani <marcello.urbani@gmail.com>
2024-12-08 18:50:54 +0000

    day5 p2

--- 2024/day05.hs
+++ 2024/day05.hs
@@ -5,6 +5,7 @@
 
 module Main where
 
+import Data.List (partition, sortBy)
 import Data.List.Split (splitOn)
 import "containers" Data.Map.Strict qualified as M
 import "containers" Data.Set qualified as S
@@ -20,11 +21,13 @@ parse s = Input (ptup <$> p1) (map read . splitOn "," <$> p2)
 precMap :: Input -> M.Map Int (S.Set Int)
 precMap (Input ps _) = M.unionsWith S.union [M.singleton b $ S.singleton a | (a, b) <- ps]
 
+mid :: [a] -> a
+mid l = l !! div (length l - 1) 2
+
 part1 :: Input -> Int
 part1 i = sum $ mid <$> valids
   where
     pmap = precMap i
-    mid l = l !! div (length l - 1) 2
     valids = filter (valid S.empty) $ iUpd i
     valid bl l = case l of
       [] -> True
@@ -34,11 +37,21 @@ part1 i = sum $ mid <$> valids
         where
           bl' = S.union bl $ M.findWithDefault S.empty x pmap
 
--- part2 :: Input -> Int
-part2 l = 0
+part2 :: Input -> Int
+part2 i = sum $ mid <$> updated
+  where
+    updated = iUpd i >>= go2
+    pmap = precMap i
+    prevs l = M.findWithDefault S.empty l pmap
+    go2 l = [l' | let l' = go [] l, l /= l']
+    go pre end = case end of
+      [] -> pre
+      x : xs -> case partition (flip S.member $ prevs x) end of
+        ([], _) -> go (pre <> [x]) xs
+        (p : _, _) -> go pre (p : filter (/= p) end)
 
 -- >>> solve $ parse "47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n75,47,61,53,29\n97,61,53,29,13\n75,29,13\n75,97,47,61,53\n61,13,29\n97,13,75,29,47"
--- (143,0)
+-- (143,123)
 
 solve l = (p1, p2)
   where
