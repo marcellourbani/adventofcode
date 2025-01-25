04e917a146c4b73a486e0390752d779097fa30e9
Marcello Urbani <marcello.urbani@gmail.com>
2025-01-25 10:02:35 +0000

    2024 day 17

--- 2024/day17.hs
+++ 2024/day17.hs
@@ -4,8 +4,8 @@
 module Main where
 
 import Data.Bits (Bits (shiftL), shiftL, shiftR, xor)
+import Data.List (intercalate)
 import Data.List.Split (splitOn)
-import Debug.Trace (trace)
 
 data OpCode = ADV | BXL | BST | JNZ | BXC | OUT | BDV | CDV deriving (Show, Eq, Enum)
 
@@ -23,7 +23,7 @@ parse s = CPU a b c 0 mem
 
 combo :: CPU -> Int -> Int
 combo cpu o
-  | o >= 0 && o <= 3 = o
+  | o <= 3 = o
   | o == 4 = cpA cpu
   | o == 5 = cpB cpu
   | o == 6 = cpC cpu
@@ -49,20 +49,38 @@ execOp cpu@(CPU a b c ip mem)
     cpu' = cpu {cpIp = ip + 2}
     adv = shiftR a $ combo cpu operand
 
-part1 cpu = case execOp cpu of
+process :: CPU -> [Int]
+process cpu = case execOp cpu of
   Nothing -> []
-  Just (cpu', [o]) -> show o <> part1 cpu'
-  Just (cpu', _) -> part1 cpu'
+  Just (cpu', [o]) -> o : process cpu'
+  Just (cpu', _) -> process cpu'
 
--- part2 :: Input -> Int
-part2 l = 0
+part2 :: CPU -> Int
+part2 cpu = go 0 $ mle - 1
+  where
+    toI (a, b) = [fromEnum a, b]
+    p8 n = floor $ 8 ^^ n
+    mem = cpMem cpu >>= toI
+    mle = length mem
+    go n dr
+      | curs == mem = n
+      | drop dr mem == curs = go (n * 8) (dr - 1)
+      | dr < 0 = undefined
+      | otherwise = go (n + 1) dr
+      where
+        -- \| otherwise = go n + inc
+        curs = process cpu {cpA = n}
+        mat = length $ takeWhile (uncurry (==)) $ zip mem curs
+        inc = p8 $ max 0 (mat - 3)
 
--- >>> solve $ parse "Register A: 729\nRegister B: 0\nRegister C: 0\n\nProgram: 0,1,5,4,3,0"
--- ("4635635210",0)
+-- >>> process $ parse "Register A: 729\nRegister B: 0\nRegister C: 0\n\nProgram: 0,1,5,4,3,0"
+-- >>> solve $ parse "Register A: 2024\nRegister B: 0\nRegister C: 0\n\nProgram: 0,3,5,4,3,0"
+-- [4,6,3,5,6,3,5,2,1,0]
+-- ("5,7,3,0",(117440,[0,3,5,4,3,0]))
 
 solve l = (p1, p2)
   where
-    p1 = part1 l
+    p1 = intercalate "," $ show <$> process l
     p2 = part2 l
 
 main :: IO ()
